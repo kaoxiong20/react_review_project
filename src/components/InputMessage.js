4024f9a15dd8163d9f720c3d911acffc9f9144e5
kaoxiong20 <kaoxiong520@gmail.com>
2023-04-09 21:04:10 -0500

    Added an event, target, and value to allow user to submit an entry to the DOM, following the recorded demo

--- src/components/InputMessage.js
+++ src/components/InputMessage.js
@@ -2,24 +2,33 @@ import React, {useState} from "react";
 
 function InputMessage(){
     const [message, setMessage] = useState('');
+    const [submittedMessage, setSubmittedMessage] = useState('');
 
     const handleInputChange = (event) => {
+        // event = the input change as the user types
+        // target = input itself
+        // value = content being typed in the input
         setMessage(event.target.value);
-        console.log(event.target.value)
+        console.log(event.target.value);
+    }
+
+    const handleSubmit = () => {
+        setSubmittedMessage(message);
+        console.log('message submitted');
+        setMessage('');
     }
 
     return(
         <>
-        <p>Temporary text</p>
+        <p>You have submitted: {submittedMessage}</p>
         <input
             type="text"
             value={message}
             onChange={handleInputChange}
         />
-        <button>Submit</button>
+        <button onClick={handleSubmit}>Submit</button>
         </>
     )
-
 }
 
 export default InputMessage;
